988ef79813c0cb332bf45a7f5682918f9d80471b
User <user@Users-MacBook-Pro-3.local>
2024-03-20 19:54:49 +0400

    backed everything right

--- test.js
+++ test.js
@@ -1,21 +1,21 @@
-// console.log("hello");
+console.log("hello");
 
 
-// const getUserFromDb = (username, onFoundCallcack) => {
+const getUserFromDb = (username, onFoundCallcack) => {
 
 
-//     setTimeout(() => {
-//         onFoundCallcack({
-//             username,
-//             psss: "12345",
-//             id:1
-//         })
+    setTimeout(() => {
+        onFoundCallcack({
+            username,
+            psss: "12345",
+            id:1
+        })
 
-//     }, 3000);
-// }
+    }, 3000);
+}
 
-// getUserFromDb("giorgi", (foundUser) => {
-//     if(foundUser.id === 1) {
-//         console.log("firstUser");
-//     }
-// });
+getUserFromDb("giorgi", (foundUser) => {
+    if(foundUser.id === 1) {
+        console.log("firstUser");
+    }
+});
